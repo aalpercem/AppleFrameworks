86af8e3d9b3faf44c7ebc0050860ca1270834876
aalpercem <aalpercem@gmail.com>
2023-09-19 16:38:17 +0300

    project creation: zero point

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Alper Cem Öztürk on 19.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
